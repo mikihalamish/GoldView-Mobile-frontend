839a96c2369d081e3ea66939050ee0fed8007c24
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 13:32:17 +0300

    departments

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Departments from "./screens/Departments";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome To GoldView Mobile!</Text>
-      <StatusBar style="auto" />
+    {/*   <Text style={styles.title}>Welcome To GoldView Mobile!</Text>
+      <StatusBar style="auto" /> */}
+      <Departments/>
     </View>
   );
 }
