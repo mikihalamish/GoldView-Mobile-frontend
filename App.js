417e3d84f6921fd6ddcee90a069fdfeb3d0032c8
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 13:36:28 +0300

    added breathing machine screen, not functional yet

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet } from "react-native";
+import BreathingMachineScreen from "./screens/BreathingMachineScreen.js";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Header } from "react-native-elements";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome To GoldView Mobile!</Text>
+    <SafeAreaProvider style={styles.container}>
+      <Header
+        centerComponent={{ text: "GoldControl", style: { color: "#fff" } }}
+      />
+      <BreathingMachineScreen />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    color: '#fff'
+    backgroundColor: "#fff"
   }
 });
