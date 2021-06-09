987490c8b1bb0485c5b76321cfb8f97d85cce8a7
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 18:48:29 +0300

    login screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet } from "react-native";
-import BreathingMachineScreen from "./screens/BreathingMachineScreen.js";
+import Login from "./screens/Login.js";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Header } from "react-native-elements";
 
@@ -11,7 +11,7 @@ export default function App() {
       <Header
         centerComponent={{ text: "GoldControl", style: { color: "#fff" } }}
       />
-      <BreathingMachineScreen />
+      <Login />
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
