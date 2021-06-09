878d6cfaa62a0b8d653c10491e87ff7333bfd30e
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 10:13:36 +0300

    Project

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome To GoldView Mobile!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    color: '#fff'
+  }
+});
